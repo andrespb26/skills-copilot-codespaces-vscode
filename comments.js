b97b877b17fcfea88fe3dc2eae378aafc01d9f8c
andrespb26 <140036874+andrespb26@users.noreply.github.com>
2023-07-21 01:53:50 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,15 @@
+// create web server
+var express = require('express');
+var router = express.Router();
+var db = require('../db');
+var ObjectId = require('mongodb').ObjectID;
+
+router.get('/', function (req, res) {
+    db.get().collection('comments').find().toArray(function (err, docs) {
+        res.render('comments', {comments: docs});
+    })
+});
+
+router.post('/', function (req, res) {
+    var comment = {
+        text: req.body.text,
